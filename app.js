f65051f4f327e72a3af99966313df6e899dc8c38
supermaxxx25-boop <supermaxxx25@icloud.com>
2026-01-28 17:18:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 Telegram.WebApp.ready()
+Telegram.WebApp.expand()
 
-Telegram.WebApp.MainButton.setText("VALIDER TEST")
-Telegram.WebApp.MainButton.show()
-
-Telegram.WebApp.MainButton.onClick(() => {
-  const testData = {
-    "Burger": 2,
-    "Pizza": 1
+function sendOrder() {
+  const data = {
+    Burger: 2,
+    Pizza: 1
   }
 
-  Telegram.WebApp.sendData(JSON.stringify(testData))
-})
+  Telegram.WebApp.sendData(JSON.stringify(data))
+}
