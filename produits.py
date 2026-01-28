8d42e6eee3e569fe81e88edc4be4e3774fd66a5a
supermaxxx25-boop <supermaxxx25@icloud.com>
2026-01-28 12:18:33 +0100

    Delete produits.py

--- produits.py
+++
@@ -1,6 +0,0 @@
-PRODUITS = {
-    1: {"nom": "T-shirt Premium", "prix": 25, "image": "https://via.placeholder.com/600x600"},
-    2: {"nom": "Casquette", "prix": 15, "image": "https://via.placeholder.com/600x600"},
-    3: {"nom": "Sac de sport", "prix": 40, "image": "https://via.placeholder.com/600x600"},
-    4: {"nom": "Sweat à capuche", "prix": 50, "image": "https://via.placeholder.com/600x600"}
-}
