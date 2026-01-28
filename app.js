0de3c85c24b152730f70f6aff1cc951a729f83a1
supermaxxx25-boop <supermaxxx25@icloud.com>
2026-01-28 16:48:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,8 @@
-let panier = {}
-
-// INITIALISATION OBLIGATOIRE
 Telegram.WebApp.ready()
 
-// CONFIGURATION DU BOUTON TELEGRAM
-Telegram.WebApp.MainButton.setText("✅ Valider la commande")
+Telegram.WebApp.MainButton.setText("TEST TELEGRAM")
 Telegram.WebApp.MainButton.show()
 
 Telegram.WebApp.MainButton.onClick(() => {
-  if (Object.keys(panier).length === 0) {
-    Telegram.WebApp.showPopup({
-      title: "Panier vide",
-      message: "Ajoute au moins un plat",
-      buttons: [{ type: "ok" }]
-    })
-    return
-  }
-
-  console.log("PANIER ENVOYÉ:", panier)
-  Telegram.WebApp.sendData(JSON.stringify(panier))
+  alert("LE BOUTON TELEGRAM FONCTIONNE ✅")
 })
-
-function add(produit) {
-  panier[produit] = (panier[produit] || 0) + 1
-
-  Telegram.WebApp.showPopup({
-    title: "Ajouté",
-    message: produit + " ajouté au panier",
-    buttons: [{ type: "ok" }]
-  })
-}
