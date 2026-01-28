3e33eb5383d7078b6f7fa618b9256f0ee7510b30
supermaxxx25-boop <supermaxxx25@icloud.com>
2026-01-28 12:02:03 +0100

    Fix import formatting in bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,7 @@
 import sys
 import os
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))import os
-
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))import threading
 import threading
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
